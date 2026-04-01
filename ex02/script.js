1ce252bc40f84d9029e38ccfbfac1c04118e751c
pedrooSoeiro <pr1004566@gmail.com>
2026-04-01 08:25:59 -0300

    Exercício 02

---
+++ ex02/script.js
@@ -0,0 +1,7 @@
+let idade = Number(prompt("Me diga sua idade por favor? (apenas números)"));
+if (idade<18){
+    alert("Você é menor de idade")
+}
+else if(idade>=18){
+    alert("Você é maior de idade")
+}
