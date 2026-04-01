cd140f32d9b888bf72ba4f059e0a84610ad35fe9
pedrooSoeiro <pr1004566@gmail.com>
2026-04-01 08:47:18 -0300

    Exercício 04

---
+++ ex04/script.js
@@ -0,0 +1,7 @@
+let valor = Number(prompt("Qual é o valor da compra?"));
+
+if (valor >= 100) {
+    alert("Você ganhou um desconto de 10%!!!");
+} else if (valor < 100){
+    alert("Não tem nenhum desconto");
+}
