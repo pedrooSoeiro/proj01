f8001d1edd8b6a9859f20694be0a55f5acc57685
pedrooSoeiro <pr1004566@gmail.com>
2026-04-01 08:46:18 -0300

    Exercício 01

--- ex01/script.js
+++ ex01/script.js
@@ -2,6 +2,9 @@ let numero = Number(prompt("Me diga um numero, por favor? :)"));
 if (numero<0){
     alert("Número negativo")
 }
-else if(numero>0){
+else if(numero === 0){
+    alert("Número neutro")
+}
+else if (numero >0){
     alert("Número positivo")
 }
